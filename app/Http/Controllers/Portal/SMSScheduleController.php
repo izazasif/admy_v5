a826d9f401149997ea74d8e5b1ee437441fec0c5
Mamunur Rahman <mamunur@Mds-MacBook-Pro.local>
2022-11-04 15:22:05 +0600

    push balance bug fix - done

--- app/Http/Controllers/Portal/SMSScheduleController.php
+++ app/Http/Controllers/Portal/SMSScheduleController.php
@@ -17,24 +17,24 @@ class SMSScheduleController extends Controller
         $is_active = "create_sms_schedule";
         $categories = Category::where('status', 1)->get();
         $sms_texts = SMSText::where('status', 1)->get();
-        $user_id = session()->get('user_id');
+        // $user_id = session()->get('user_id');
         //get sms balance
         //---------------
         // $user_credit = UserSMS::where(['user_id'=>$user_id,'is_active'=>1])->where('valid_till', '>=', date('Y-m-d H:i:s'))->where('status', 1)->sum('amount');
         // $user_debit = SMSSchedule::where('user_id', $user_id)->sum('sms_amount');
         // $user_remaining = $user_credit-$user_debit;
-        $push_debit = SMSSchedule::where('user_id', $user_id)->sum('sms_amount');
-        $push_invalid = UserSMS::where(['user_id'=>$user_id, 'is_active'=>1, 'status'=>1])->where('valid_till', '<', date('Y-m-d H:i:s'))->sum('amount');
-        $push_valid = UserSMS::where(['user_id'=>$user_id, 'is_active'=>1, 'status'=>1])->where('valid_till', '>=', date('Y-m-d H:i:s'))->sum('amount');
-        $temp = $push_debit - $push_invalid;
-        if($temp <= 0){
-          $user_remaining = $push_valid;
-        }else{
-          $user_remaining = $push_valid - $temp;
-        }
+        // $push_debit = SMSSchedule::where('user_id', $user_id)->sum('sms_amount');
+        // $push_invalid = UserSMS::where(['user_id'=>$user_id, 'is_active'=>1, 'status'=>1])->where('valid_till', '<', date('Y-m-d H:i:s'))->sum('amount');
+        // $push_valid = UserSMS::where(['user_id'=>$user_id, 'is_active'=>1, 'status'=>1])->where('valid_till', '>=', date('Y-m-d H:i:s'))->sum('amount');
+        // $temp = $push_debit - $push_invalid;
+        // if($temp <= 0){
+        //   $user_remaining = $push_valid;
+        // }else{
+        //   $user_remaining = $push_valid - $temp;
+        // }
+        // session()->put('user_sms_credit', $user_remaining);
         //---------------
         //get sms balance
-        session()->put('user_sms_credit', $user_remaining);
 
         return view('portal.sms_schedule.create', compact('title', 'is_active','categories','sms_texts'));
     }
@@ -62,13 +62,19 @@ class SMSScheduleController extends Controller
 
         $this->validate($request, $rules, $messages);
         $user_id = session()->get('user_id');
-        $user_credit = UserSMS::where(['user_id'=>$user_id,'is_active'=>1])->where('valid_till', '>=', date('Y-m-d H:i:s'))->where('status', 1)->sum('amount');
+
+        $userPushSMSBalance = getUserPushSMSBalance($user_id);
         $req_amount = $request->sms_amount;
-        $user_debit = SMSSchedule::where('user_id', $user_id)->sum('sms_amount');
-        $left = $user_debit+$req_amount;
-        if($user_credit<$left){
+        if($req_amount > $userPushSMSBalance){
             return redirect()->back()->withErrors("Please buy a package, You have no available sms credit");
         }
+
+        // $user_credit = UserSMS::where(['user_id'=>$user_id,'is_active'=>1])->where('valid_till', '>=', date('Y-m-d H:i:s'))->where('status', 1)->sum('amount');
+        // $user_debit = SMSSchedule::where('user_id', $user_id)->sum('sms_amount');
+        // $left = $user_debit+$req_amount;
+        // if($user_credit<$left){
+        //     return redirect()->back()->withErrors("Please buy a package, You have no available sms credit");
+        // }
 
 
         $schedule_datetime = date('Y-m-d H:i:s', strtotime($request->schedule_datetime));
@@ -91,9 +97,12 @@ class SMSScheduleController extends Controller
         $scheduleData->remark = $request->remark;
         $scheduleData->save();
 
-        $user_debit = SMSSchedule::where('user_id', $user_id)->sum('sms_amount');
-        $user_remaining = $user_credit-$user_debit;
-        session()->put('user_sms_credit', $user_remaining);
+        // $user_debit = SMSSchedule::where('user_id', $user_id)->sum('sms_amount');
+        // $user_remaining = $user_credit-$user_debit;
+        // session()->put('user_sms_credit', $user_remaining);
+
+        $userPushSMSBalance = getUserPushSMSBalance($user_id);
+        session()->put('user_sms_credit', $userPushSMSBalance);
 
         $message = 'Schedule saved successfully! In case of technical difficulties, we will deliver your schedule within 72 hours.';
 
