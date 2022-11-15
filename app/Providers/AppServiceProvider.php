61e72bb1849a1541a1f7d3696ac354e4a7517c30
tanvira1993 <tanvira1993@gmail.com>
2022-11-15 13:15:46 +0600

    bug fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if(config('app.env') === 'production') {
-            \URL::forceScheme('http');
+            \URL::forceScheme('https');
         }
     }
 }
