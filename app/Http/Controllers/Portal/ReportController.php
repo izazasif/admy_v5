319124d59e304b814a44d26db8dff2c473bbfe4c
tanvira1993 <tanvira1993@gmail.com>
2022-11-23 16:04:37 +0600

    user sms is active query fix

--- app/Http/Controllers/Portal/ReportController.php
+++ app/Http/Controllers/Portal/ReportController.php
@@ -29,14 +29,14 @@ class ReportController extends Controller
     public function dailySmsReport(){
         $title = "AdMy | Daily Report";
         $is_active = "sms_report";
-        $data = UserSMS::select(DB::raw('DATE(created_at) as date'),DB::raw('count(*) as total'))->where('status',1)->groupBy('date')->orderBy('date','DESC')->paginate(20);       
+        $data = UserSMS::select(DB::raw('DATE(created_at) as date'),DB::raw('count(*) as total'))->where('status',1)->where('is_active',1)->groupBy('date')->orderBy('date','DESC')->paginate(20);       
         return view('portal.report.smsreport', compact('title','is_active','data'));
     }
 
     public function activityLog(){
         $title = "AdMy | Activity Log";
         $is_active = "log_report";
-        $data = Log::select('logs.*','users.email','users.username')->leftJoin('users','users.id','=','logs.user_id')->paginate(15);
+        $data = Log::select('logs.*','users.email','users.username')->leftJoin('users','users.id','=','logs.user_id')->orderBy('id','DESC')->paginate(5);
         return view('portal.report.activityLog', compact('title','is_active','data'));
     }
 }
